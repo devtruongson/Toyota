f7e9566c71f7a9b80a02a9d3dc12aa3fdb9fa268
Truong Son <truongsonpt.80@gmail.com>
2025-02-07 13:00:13 +0700

    first commit

---
+++ backend-system/src/server.ts
@@ -0,0 +1,16 @@
+import compression from 'compression';
+import express, { Request, Response } from 'express';
+import connectDB from './configs/connectDB';
+
+const app = express();
+app.use(compression());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('test'.repeat(100));
+});
+
+connectDB();
+
+app.listen(8081, () => {
+    console.log('App starting successfully with port 8081');
+});
